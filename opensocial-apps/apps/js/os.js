058b19f42c784ac31d18a41b66263577c4b0238a
justin.kruger@gmail.com <justin.kruger@gmail.com@c1c3ef76-c69c-b4fd-be1b-adf864619b08>
2010-11-02 22:39:22 +0000

    Added HideShow.xml , a demo gadget to test and maintain the hide show functionality of future gadgets.

--- opensocial-apps/apps/js/os.js
+++ opensocial-apps/apps/js/os.js
@@ -7,6 +7,10 @@ if( typeof(console) == "undefined" ){ window.console = { "log" :function(){}, "w
 window.os = window.os || {}; 	//opensocial helpers
 os.isReady = false;		//need to wait for the container to load **slow
 os.readyList = [];
+os.Viewer = os.Viewer || {};
+os.Owner = os.Owner || {};
+
+os.osapi = os.osapi || {};
 
 os.makeReady = function(){
 	os.isReady = true;
@@ -24,6 +28,34 @@ os.execReady = function(){
 	}    
 }
 
+os.init = function(){
+	try{
+		var batch = osapi.newBatch().
+		    add("viewer", osapi.people.getViewer() ).
+		    add("owner", osapi.people.getOwner() );
+		    
+		    batch.execute(function(result){
+		    	//TODO: check result for error
+		    	
+				  console.log('@viewer', result.viewer);
+				  os.Viewer = result.viewer;
+				  
+				  console.log('@owner', result.owner);
+				  os.Owner = result.owner;
+				  
+				  os.initComplete();
+				});
+	
+	}catch(e){
+		console.log('os.init failed', e);
+	}
+	
+};
+
+os.initComplete = function(){
+	os.makeReady();
+};
+
 /**
 wrapper function that only executes when opensocial is ready.
 */
@@ -37,6 +69,26 @@ os.ready = function(fn){
 }
 
 gadgets.util.registerOnLoadHandler(os.makeReady);
+// gadgets.util.registerOnLoadHandler(os.init);  //use this to force the @owner and @viewer to be set., makes 2 web requests though ;-(
+
+os.osapi._getFirstPerson = function(result){
+	var person = {};
+	for( p in result){
+		person = result[p];
+		break;
+	}
+	return person;
+};
+
+os.osapi.getViewerFromResult = function(result){
+	return os.osapi._getFirstPerson(result);
+};
+
+os.osapi.getOwnerFromResult = function(result){
+	return os.osapi._getFirstPerson(result);
+};
+
+
 
 os.ready( function(){
 	gadgets.window.adjustHeight();

1a9b198b821021bd0a9b20a6bad2d19b622b1ca9
alexnv@sbcglobal.net <alexnv@sbcglobal.net@c1c3ef76-c69c-b4fd-be1b-adf864619b08>
2011-07-01 18:25:38 +0000

    fixed a few typos in the code

--- opensocial-apps/chatter/db/sp_ExportActivitiesToChattrer.sql
+++ opensocial-apps/chatter/db/sp_ExportActivitiesToChattrer.sql
@@ -24,10 +24,10 @@ BEGIN
 	declare @userId int
 	declare @createdDT datetime
 	declare @activity xml
-	declare @emploeeyId nvarchar(50)
+	declare @employeeId nvarchar(50)
 	
 	DECLARE activityCursor CURSOR FAST_FORWARD FOR 
-	SELECT TOP 100 a.activityId, a.userId, a.createdDT, a.activity, u.InternalUserName as emploeeyId
+	SELECT TOP 100 a.activityId, a.userId, a.createdDT, a.activity, u.InternalUserName as employeeId
 	FROM [shindig_activity] a INNER JOIN [dbo].[user] u ON a.[userId] = u.[UserID]
 	WHERE chatterFlag = 0
 	ORDER BY userId
@@ -35,16 +35,16 @@ BEGIN
 	OPEN activityCursor
 
 	FETCH NEXT FROM activityCursor 
-	INTO @activityId, @userId, @createdDT, @activity, @emploeeyId
+	INTO @activityId, @userId, @createdDT, @activity, @employeeId
 
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
 
-		exec dbo.usp_CreateChatterActivity @url, @username, @password, @token, @emploeeyId, @activity
+		exec dbo.usp_CreateChatterActivity @url, @username, @password, @token, @employeeId, @activity
 		UPDATE shindig_activity SET chatterFlag = 1 WHERE activityId = @activityId
 		
 		FETCH NEXT FROM activityCursor 
-		INTO @activityId, @userId, @createdDT, @activity, @emploeeyId
+		INTO @activityId, @userId, @createdDT, @activity, @employeeId
 	END
 	
 	CLOSE activityCursor

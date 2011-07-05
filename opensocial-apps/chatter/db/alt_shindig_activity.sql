1b1e1bb1efcde5366a952361a6808de5bd702f50
alexnv@sbcglobal.net <alexnv@sbcglobal.net@c1c3ef76-c69c-b4fd-be1b-adf864619b08>
2011-07-05 04:26:42 +0000

    modified shindig_activity table

--- opensocial-apps/chatter/db/alt_shindig_activity.sql
+++ opensocial-apps/chatter/db/alt_shindig_activity.sql
@@ -1,8 +1,23 @@
 
-IF NOT EXISTS(SELECT [name] FROM syscolumns WHERE [name] = 'chatterFlag' AND id = OBJECT_ID('shindig_activity')) BEGIN
-	ALTER TABLE shindig_activity ADD chatterFlag bit NULL DEFAULT 0
-END
+IF EXISTS(SELECT [name] FROM syscolumns WHERE [name] = 'chatterFlag' AND id = OBJECT_ID('shindig_activity')) BEGIN
+	EXEC sp_rename 'shindig_activity.chatterFlag', 'chatterFlagOld', 'COLUMN'
+END 
 GO
 
-UPDATE shindig_activity SET chatterFlag = 0 where chatterFlag is null
-ALTER TABLE shindig_activity ALTER COLUMN chatterFlag bit NOT NULL
+IF NOT EXISTS(SELECT [name] FROM syscolumns WHERE [name] = 'chatterFlag' AND id = OBJECT_ID('shindig_activity')) BEGIN
+	ALTER TABLE shindig_activity ADD chatterFlag char NULL
+END 
+GO
+
+UPDATE shindig_activity SET chatterFlag = 'S' where chatterFlagOld = 1 and chatterFlag is null
+
+IF NOT EXISTS(SELECT [name] FROM syscolumns WHERE [name] = 'chatterAttempts' AND id = OBJECT_ID('shindig_activity')) BEGIN
+	ALTER TABLE shindig_activity ADD chatterAttempts int NULL
+END 
+GO
+
+IF NOT EXISTS(SELECT [name] FROM syscolumns WHERE [name] = 'updatedDT' AND id = OBJECT_ID('shindig_activity')) BEGIN
+	ALTER TABLE shindig_activity ADD updatedDT datetime NULL
+END 
+GO
+
--- opensocial-apps/chatter/db/sp_ExportActivitiesToChattrer.sql
+++ opensocial-apps/chatter/db/sp_ExportActivitiesToChattrer.sql
@@ -4,6 +4,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[sp_ExportActivitiesToChattrer]') AND type in (N'P', N'PC')) BEGIN
+	DROP PROCEDURE [dbo].[sp_ExportActivitiesToChattrer]
+END
+GO
+
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
@@ -24,29 +30,57 @@ BEGIN
 	declare @userId int
 	declare @createdDT datetime
 	declare @activity xml
-	declare @employeeId nvarchar(50)
+	declare @emploeeyId nvarchar(50)
+	declare @attempts int
+	declare @errorCount int
+	declare @errorMsg nvarchar(max)
+	
+	set @errorCount = 0
 	
 	DECLARE activityCursor CURSOR FAST_FORWARD FOR 
-	SELECT TOP 100 a.activityId, a.userId, a.createdDT, a.activity, u.InternalUserName as employeeId
+	SELECT TOP 100 a.activityId, a.userId, a.createdDT, a.activity, a.chatterAttempts ,u.InternalUserName as emploeeyId
 	FROM [shindig_activity] a INNER JOIN [dbo].[user] u ON a.[userId] = u.[UserID]
-	WHERE chatterFlag = 0
+	WHERE chatterFlag is null
 	ORDER BY userId
 
 	OPEN activityCursor
 
 	FETCH NEXT FROM activityCursor 
-	INTO @activityId, @userId, @createdDT, @activity, @employeeId
+	INTO @activityId, @userId, @createdDT, @activity, @attempts, @emploeeyId
 
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
-
-		exec dbo.usp_CreateChatterActivity @url, @username, @password, @token, @employeeId, @activity
-		UPDATE shindig_activity SET chatterFlag = 1 WHERE activityId = @activityId
+		BEGIN TRY
+			set @attempts = ISNULL(@attempts, 0) + 1
+			
+			exec dbo.usp_CreateChatterActivity @url, @username, @password, @token, @emploeeyId, @activity
+			UPDATE shindig_activity SET chatterFlag = 'S', chatterAttempts = @attempts, updatedDT = GETDATE() WHERE activityId = @activityId
+		END TRY
+		BEGIN CATCH
+			set @errorCount = @errorCount + 1
+			select @errorMsg = ERROR_MESSAGE()
+			RAISERROR (@errorMsg, 10, 1)
+			
+			IF @attempts > 10 BEGIN
+				UPDATE shindig_activity SET chatterFlag = 'F', chatterAttempts = @attempts, updatedDT = GETDATE() WHERE activityId = @activityId
+			END	
+			ELSE BEGIN
+				UPDATE shindig_activity SET chatterAttempts = @attempts, updatedDT = GETDATE() WHERE activityId = @activityId
+			END
+						 
+			IF @errorCount > 10 BEGIN
+				RAISERROR ('Too many errors', 11,1)
+				CLOSE activityCursor
+				DEALLOCATE activityCursor
+				RETURN
+			END
+		END CATCH
 		
 		FETCH NEXT FROM activityCursor 
-		INTO @activityId, @userId, @createdDT, @activity, @employeeId
+		INTO @activityId, @userId, @createdDT, @activity, @attempts, @emploeeyId
 	END
 	
 	CLOSE activityCursor
 	DEALLOCATE activityCursor
 END
+GO

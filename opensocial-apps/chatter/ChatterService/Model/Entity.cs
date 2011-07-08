f5795b2482e6c1b9ad42682b3f26e2f71deb2a34
alexnv@sbcglobal.net <alexnv@sbcglobal.net@c1c3ef76-c69c-b4fd-be1b-adf864619b08>
2011-07-08 21:18:48 +0000

    - added ChatterServiceWeb - fixed problem with CreatedDate field

---
+++ opensocial-apps/chatter/ChatterService/Model/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace ChatterService.Model
+{
+    [DataContract(IsReference = false)]
+    [KnownType(typeof(User))]
+    public class Entity
+    {
+        [DataMember]
+        public string Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
---
+++ opensocial-apps/chatter/ChatterService/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace ChatterService.Model
+{
+    [DataContract(IsReference = false)]
+    class User : Entity
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string EmployeeId { get; set; }
+    }
+}

ff988d33b541e2ce973ccec2f82cf24a93cd867c
Alexei Vassiliev <avassiliev@sbcglobal.net>
2012-05-15 14:41:51 -0700

    added configuration parameters for URL, delay and HTTP headers

--- opensocial-apps/grantloader/UCSF.Business/Web/GrantOnlineValidator.cs
+++ opensocial-apps/grantloader/UCSF.Business/Web/GrantOnlineValidator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -7,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using UCSF.Data;
 using UCSF.Data.Comparers;
+using UCSF.Framework.Utils;
 using log4net;
 
 namespace UCSF.Business.Web
@@ -38,16 +40,38 @@ namespace UCSF.Business.Web
             private set { errorsCount = value; }
         }
 
+        public string ValidationUrl { get; private set; }
+        public string ValidationPattern { get; private set; }
+
         public void ValidateGrants()
         {
+            GetSettings();
+
             ucsdDataContext = new UCSDDataContext();
             var grants = ucsdDataContext.Grants.Where(g => g.IsVerified == null && g.GrantPrincipals.Any(
                         gp => gp.PrincipalInvestigator != null && gp.PrincipalInvestigator.EmployeeId != null));
 
+            int timeout = getTimeout();
             foreach (Grant grant in grants.ToList().Distinct(new GrantAppIdComparer()))
             {
                ValidateGrantOnline(grant);
+               Thread.Sleep(timeout*1000);
             }
+        }
+
+        private void GetSettings()
+        {
+            ValidationUrl = ConfigurationManager.AppSettings["GrantValidation.Url"];
+            ValidationPattern = ConfigurationManager.AppSettings["GrantValidation.Pattern"];
+        }
+
+        private int getTimeout() {
+            string timeout = ConfigurationManager.AppSettings["GrantValidation.Timeout"];
+            if (!String.IsNullOrWhiteSpace(timeout))
+            {
+                return Int32.Parse(timeout);
+            }
+            return 10;
         }
 
         private void ValidateGrantOnline(Grant grant)
@@ -56,15 +80,16 @@ namespace UCSF.Business.Web
 
             try
             {
-                WebClient wc = new WebClient();
-                using (MemoryStream ms = new MemoryStream(wc.DownloadData(string.Format("http://projectreporter.nih.gov/project_info_description.cfm?aid={0}",
-                                              grant.ApplicationId))))
+                WebClientEx wc = new WebClientEx();
+                wc.Headers.Add(HttpRequestHeader.UserAgent, ConfigurationManager.AppSettings["GrantValidation.Header"]);
+                                
+                using (MemoryStream ms = new MemoryStream(wc.DownloadData(string.Format(ValidationUrl, grant.ApplicationId))))
                 {
                     ms.Seek(0, SeekOrigin.Begin);
                     StreamReader sb = new StreamReader(ms);
                     string doc = sb.ReadToEnd();
 
-                    grant.IsVerified = doc.IndexOf("This project doesn't exist in RePORTER") < 0;
+                    grant.IsVerified = doc.IndexOf(ValidationPattern) < 0;
                     
                     ucsdDataContext.SubmitChanges();
 
@@ -82,7 +107,8 @@ namespace UCSF.Business.Web
             }
             catch(Exception ex)
             {
-                log.Error("Grant validation error", ex);
+                log.Error(String.Format("Grant validation error: {0}", ex.Message));
+                log.Debug("Error", ex);
 
                 Interlocked.Increment(ref errorsCount);
             }
---
+++ opensocial-apps/grantloader/UCSF.Framework/Utils/WebClientEx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace UCSF.Framework.Utils
+{
+    public class WebClientEx : WebClient
+    {
+        private int _timeout = 100;
+        public int Timeout
+        {
+            get { return _timeout; }
+            set { _timeout = value; }
+        }
+
+        private HttpWebRequest webRequest;
+        public HttpWebRequest WebRequest
+        {
+            get { return webRequest; }
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            webRequest = base.GetWebRequest(address) as HttpWebRequest;
+
+            if (webRequest != null)
+            {
+                webRequest.Timeout = Timeout * 1000;
+            }
+
+            return webRequest;
+        }
+         
+    }
+}

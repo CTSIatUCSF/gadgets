9762a5abf1efee561cc35df80bc6a8281422d522
Eric Meeks <eric.meeks@ucsf.edu>
2013-09-05 16:21:19 -0700

    New structure and new ability to pull in environment (stage, production, etc) info

---
+++ opensocial-apps/js/environment.js
@@ -0,0 +1,13 @@
+// UNCOMMENT OUT THE BLOCK THAT IS APPROPRIATE FOR YOUR ENVIRONMENT!!!!
+
+//For production
+/************
+var ENV_PROFILES_URL = 'http://profiles.ucsf.edu';
+var ENV_LOCAL_URL = 'http://profiles.ucsf.edu';
+**************/
+
+//For staging
+/************
+var ENV_PROFILES_URL = 'http://stage-profiles.edu/profiles200';
+var ENV_LOCAL_URL = 'http://stage-profiles.campus.net.ucsf.edu';
+**************/

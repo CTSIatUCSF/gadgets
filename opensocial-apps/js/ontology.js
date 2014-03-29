b88cac10fb204e126bc763e01f4868a0ef539965
Eric Meeks <eric.meeks@ucsf.edu>
2014-03-29 11:49:42 -0700

    Improved jsonld parsing

--- opensocial-apps/js/ontology.js
+++ opensocial-apps/js/ontology.js
@@ -1,22 +1,22 @@
 // figure out a way to generate this!
-var RDFS = {};
-RDFS.ns = "http://www.w3.org/2000/01/rdf-schema#";
-RDFS.label = RDFS.ns + "label;"
 
-var FOAF = {}
-FOAF.ns = "http://xmlns.com/foaf/0.1/";
-FOAF.firstName = FOAF.ns + "firstName";
-FOAF.lastName = FOAF.ns + "lastName";
+var RDFS = function(property) {
+	return "http://www.w3.org/2000/01/rdf-schema#" + property;	
+}
 
-var VIVO = {};
-VIVO.ns = "http://vivoweb.org/ontology/core#";
-VIVO.email = VIVO.ns + "email";
-VIVO.preferredTitle = VIVO.ns + "preferredTitle";
-VIVO.authorInAuthorship = VIVO.ns + "authorInAuthorship";
-VIVO.linkedInformationResource = VIVO.ns + "linkedInformationResource";
+var BIBO = function(property) {
+	return "http://purl.org/ontology/bibo/" + property;	
+}
 
-var PRNS = {};
-PRNS.ns = "http://profiles.catalyst.harvard.edu/ontology/prns#";
-PRNS.fullName = PRNS.ns + "fullName";
-PRNS.coAuthorOf = PRNS.ns + "coAuthorOf";
-PRNS.similarTo = PRNS.ns + "similarTo";
+var FOAF = function(property) {
+	return "http://xmlns.com/foaf/0.1/" + property;	
+}
+
+var VIVO = function(property) {
+	return "http://vivoweb.org/ontology/core#" + property;
+};
+
+var PRNS = function(property) {
+	return "http://profiles.catalyst.harvard.edu/ontology/prns#" + property;
+};
+

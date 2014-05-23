b05e8850e4abe9a2e4b90e26f1eef127a08284ba
Eric Meeks <eric.meeks@ucsf.edu>
2014-05-23 13:55:18 -0700

    Look and feel changes

--- opensocial-apps/js/environment.js
+++ opensocial-apps/js/environment.js
@@ -9,3 +9,45 @@ var ENV_LOCAL_URL = 'http://profiles.ucsf.edu';
 //For staging
 var ENV_PROFILES_URL = 'http://stage-profiles.ucsf.edu/profiles200';
 var ENV_LOCAL_URL = 'http://stage-profiles.ucsf.edu';
+
+//Institution variables
+var ENV_INST_ABRV = 'UCSF';
+var ENV_INST_EMAIL = "<a href='mailto:profiles@ucsf.edu'>profiles@ucsf.edu</a>";
+var ENV_INST_PUBSCONTACT = "<a href='mailto:profiles@ucsf.edu?subject=Awarded Grant Information in UCSF Profiles'>contact us</a>";
+
+//Mentor gadget variables
+var ENV_MENTOR_EDIT = "Add details about your availability to mentor UCSF faculty. Learn about the <a href='http://academicaffairs.ucsf.edu/ccfl/faculty_mentoring_program.php' target='_blank' title='Go to the UCSF Faculty Mentoring Website'>Faculty Mentoring Program</a> and the <a href='http://ctsi.ucsf.edu/training/mdp-announcement' target='_blank' title='Go to the CTSI MDP Web page'>CTSI&nbsp;Mentor&nbsp;Development&nbsp;Program</a>";
+
+var ENV_MENTOR_DEFS = "<a href='http://ctsi.ucsf.edu/training/mdp-seminar1-definitions' target='_blank' title='Go to the Mentor Role Definitions Web page' onClick=\"gadgetEventTrack('view_mentor_roles', 'http://ctsi.ucsf.edu/training/mdp-seminar1-definitions'); return true\">Review Mentor Role Definitions</a>";
+
+var ENV_MENTOR_EXAMPLES = "EXAMPLE 1:<br />Dr. Brown is willing to mentor faculty, fellows, have "
+  + "residents and students interested in an academic research career.  Most often her mentees "
+  + "had training in clinical research methods or will obtain training through the CTSI CTST. For "
+  + "students, it is expected they will have dedicated time for research. Through email or meeting, "
+  + "if there is a &quot;match&quot; for research interest, time, and training, further discussions "
+  + "as to project, goals, and access to resources (space, databases, and  statistical support) "
+  + "will be discussed to provide a productive experience.<br><br>EXAMPLE 2:<br />"
+  + "I am highly qualified to participate as a lead mentor or co-mentor at UCSF. My program of "
+  + "research is focused on health outcomes associated with disturbed sleep in various populations "
+  + "of healthy women and women with chronic illnesses like HIV/AIDS and cancer.  I have completed "
+  + "cross-sectional studies, longitudinal studies, and most recently, randomized clinical trials "
+  + "to improve sleep.  I have mentored doctoral students and postdoctoral fellows studying various "
+  + "patient populations, from the very young to very old.  During my tenure at UCSF, I have directly "
+  + "supervised over 30 doctoral students, mentored 14 postdoctoral trainees, and served as a lead "
+  + "mentor for 6 pre-tenured faculty. I have been the Director for a T32 Nurse Research Training "
+  + "Grant since 1996, and I have been honored with being voted mentor of the year by doctoral "
+  + "students on two occasions.  I play a significant role in the clinical and translational (CTSI) "
+  + "research mentoring and career development programs at UCSF.  I am the seminar leader for the first "
+  + "session in the CTSI Mentor Development program on &quot;Rewards and Challenges of Mentoring&quot; "
+  + "and I have co-mentored two KL2 scholars.  I have published over 50 peer-reviewed research "
+  + "articles with trainees as first-author, and serve as a consultant on two external K awards as "
+  + "well as three external R01 awards with former mentees. Finally, I have served on many different "
+  + "NIH study section review panels, and I served as the Chair of an NIH study section (2008-2010),"
+  + "which allows me to be particularly effective in mentoring early career principal investigators "
+  + "who are writing their first NIH applications.<br><br>"
+  + "I can provide mentees a cubicle space with my research team, tangible resources such as access "
+  + "to large datasets for secondary analysis as needed, and intangible resources such as attending "
+  + "our formal research team meetings and our informal spontaneous group discussions as well as "
+  + "networking at national sleep research conferences.";
+
+var ENV_MENTOR_LEARN = "Learn about the <a href='http://academicaffairs.ucsf.edu/ccfl/faculty_mentoring_program.php' target='_blank' title='Go to the UCSF Faculty Mentoring Website' onClick=\"gadgetEventTrack('go_to_program', 'http://academicaffairs.ucsf.edu/ccfl/faculty_mentoring_program.php'); return true\">Faculty Mentoring Program</a> and the <a href='http://ctsi.ucsf.edu/training/mdp-announcement' target='_blank' title='Go to the CTSI MDP Web page' onClick=\"gadgetEventTrack('go_to_development', 'http://ctsi.ucsf.edu/training/mdp-announcement'); return true\">CTSI Mentor Development Program</a>";

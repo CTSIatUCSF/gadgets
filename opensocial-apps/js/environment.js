589658fa94447f4d41e95458ad8471293c14a542
Eric Meeks <eric.meeks@ucsf.edu>
2014-11-19 15:19:39 -0800

    UCSD environment variables

--- opensocial-apps/js/environment.js
+++ opensocial-apps/js/environment.js
@@ -7,10 +7,11 @@ var ENV_LOCAL_URL = 'http://profiles.ucsf.edu';
 **************/
 
 //For staging
-var ENV_PROFILES_URL = 'http://stage-profiles.ucsf.edu/profiles200';
-var ENV_LOCAL_URL = 'http://stage-profiles.ucsf.edu';
+var ENV_PROFILES_URL = 'http://stage-profiles.ucsd.edu';
+var ENV_LOCAL_URL = 'http://profiles.ucsd.edu';
 
 //Institution variables
-var ENV_INST_ABRV = 'UCSF';
-var ENV_INST_EMAIL = "<a href='mailto:profiles@ucsf.edu'>profiles@ucsf.edu</a>";
-var ENV_INST_PUBSCONTACT = "<a href='mailto:profiles@ucsf.edu?subject=Awarded Grant Information in UCSF Profiles'>contact us</a>";
+var ENV_INST_ABRV = 'UCSD';
+var ENV_INST_EMAIL = "<a href='mailto:ctri-support@ucsd.edu'>mailto:ctri-support@ucsd.edu</a>";
+// This next one is gadget specific and should not be in this file
+// var ENV_INST_PUBSCONTACT = "<a href='mailto:profiles@ucsf.edu?subject=Awarded Grant Information in UCSF Profiles'>contact us</a>";

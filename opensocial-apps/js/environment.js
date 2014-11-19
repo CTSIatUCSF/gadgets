b381b229f204b2c770af5a4f772126a59dd743b1
Eric Meeks <eric.meeks@ucsf.edu>
2014-11-19 15:12:52 -0800

    Edited environment variables

--- opensocial-apps/js/environment.js
+++ opensocial-apps/js/environment.js
@@ -11,6 +11,7 @@ var ENV_PROFILES_URL = 'http://profilesstage.sc-ctsi.org';
 var ENV_LOCAL_URL = 'http://profilesstage.sc-ctsi.org';
 
 //Institution variables
-var ENV_INST_ABRV = 'UCSF';
-var ENV_INST_EMAIL = "<a href='mailto:profiles@ucsf.edu'>profiles@ucsf.edu</a>";
-var ENV_INST_PUBSCONTACT = "<a href='mailto:profiles@ucsf.edu?subject=Awarded Grant Information in UCSF Profiles'>contact us</a>";
+var ENV_INST_ABRV = 'USC';
+var ENV_INST_EMAIL = "<a href='mailto:profiles@sc-ctsi.org'>profiles@sc-ctsi.org</a>";
+// This next line is specific to a single gadget and should probably not be in here
+//var ENV_INST_PUBSCONTACT = "<a href='mailto:profiles@ucsf.edu?subject=Awarded Grant Information in UCSF Profiles'>contact us</a>";

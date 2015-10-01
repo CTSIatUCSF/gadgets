df8e541e1963304c87ec428e09dfdbaa5b42534c
cpiontko <cynthia.piontkowski@ucsf.edu>
2015-10-01 14:59:10 -0700

    UCSF colors & Clinical Trials vars
    also added missing Google Analytics tracking codes & ENV_STATS_PATH to
    environment.js

--- opensocial-apps/js/environment.js
+++ opensocial-apps/js/environment.js
@@ -4,11 +4,19 @@
 /************
 var ENV_PROFILES_URL = 'http://profiles.ucsf.edu';
 var ENV_LOCAL_URL = 'http://profiles.ucsf.edu';
+var ENV_GA_TRACKING_CODE = 'UA-1142388-6';
+var ENV_GADGET_URL = 'http://profiles.ucsf.edu/apps_ucsf';
 **************/
 
 //For staging
 var ENV_PROFILES_URL = 'http://stage-profiles.ucsf.edu/profiles200';
 var ENV_LOCAL_URL = 'http://stage-profiles.ucsf.edu';
+var ENV_GA_TRACKING_CODE = 'UA-1142388-20';
+var ENV_STATS_PATH = 'http://stage-profiles.ucsf.edu/apps_ucsf';
+var ENV_GADGET_URL = 'http://stage-profiles.ucsf.edu/apps_ucsf';
+
+// for ClinicalTrials
+var ENV_API_URL = 'http://base.ctsi.ucsf.edu/experiments/clinical_trials_gadget_api/';
 
 //Institution variables
 var ENV_INST_ABRV = 'UCSF';

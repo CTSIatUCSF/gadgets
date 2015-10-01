7ad0783a9f986235800f302bd2a62c3edec584ee
Eric Meeks <eric.meeks@ucsf.edu>
2015-10-01 15:54:59 -0700

    Gadget changes

--- opensocial-apps/js/environment.js
+++ opensocial-apps/js/environment.js
@@ -9,6 +9,9 @@ var ENV_LOCAL_URL = 'http://profiles.ucsf.edu';
 //For staging
 var ENV_PROFILES_URL = 'http://stage-profiles.ucsf.edu/profiles200';
 var ENV_LOCAL_URL = 'http://stage-profiles.ucsf.edu';
+// for ClinicalTrials
+var ENV_GADGET_URL = 'http://sc-ctsi.org/clinical_trials_gadget';
+var ENV_API_URL = 'http://base.ctsi.ucsf.edu/experiments/clinical_trials_gadget_api/';
 
 //Institution variables
 var ENV_INST_ABRV = 'UCSF';

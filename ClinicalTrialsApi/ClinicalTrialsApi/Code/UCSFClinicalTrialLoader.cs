dd13711053bf4e08fdad38e3549de14fc60f70f8
Meeks <eric.meeks@ucsf.edu>
2020-05-28 08:05:19 -0700

    Changes to support UCLA

--- ClinicalTrialsApi/ClinicalTrialsApi/Code/UCSFClinicalTrialLoader.cs
+++ ClinicalTrialsApi/ClinicalTrialsApi/Code/UCSFClinicalTrialLoader.cs
@@ -49,7 +49,7 @@ namespace ClinicalTrialsApi
                         CompletionDate = trial.Value.completion_date != null? DateTime.ParseExact(trial.Value.completion_date, DATE_FORMAT, CultureInfo.InvariantCulture) : null,
                         Status = trial.Value.recruitment_status,//TODO: need to use correct property
                         Conditions =  conditions.ToString(),
-                        SourceUrl = isVisible ? "https://clinicaltrials." + domainName  + "/trial/" + trial.Value.id : "https://clinicaltrials.gov/ct2/show/" + trial.Value.id
+                        SourceUrl = isVisible ? "https://clinicaltrials." + domainName + "/trial/" + trial.Value.id : "https://clinicaltrials.gov/ct2/show/" + trial.Value.id
                     };
                     ClinicalTrials.Add(clinicalTrial.Id, clinicalTrial);
 
